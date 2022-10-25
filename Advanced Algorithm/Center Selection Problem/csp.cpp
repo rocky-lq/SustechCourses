a743c85b78120540a2dbc5b1fb7a31de61c6f96d
rocky-lq <rocky_lq@icloud.com>
2022-10-25 13:03:25 +0800

    finish homework 6

--- Advanced Algorithm/Center Selection Problem/csp.cpp
+++ Advanced Algorithm/Center Selection Problem/csp.cpp
@@ -12,16 +12,16 @@ using namespace std;
 #define eps 1e-6
 
 double threshold = 0.9;
-int n = 20;
-int k = 5;
+int n = 9;
+int k = 4;
 int low = 0;
-int high = 10;
+int high = 8;
 int epoch = 200;
 
 random_device rd;
 mt19937 gen(rd());
 // 随机数生成器，用于生成site坐标。
-uniform_int_distribution<int> distribution_coordinates(low, high);
+uniform_int_distribution<int> distribution_coordinates(low + 1, high - 1);
 // 随机数生成器，用于随机选择起点。
 uniform_int_distribution<int> distribution_index(0, n - 1);
 
@@ -92,43 +92,52 @@ int select_first_site(vector<Tuple> tuples, int tuple_num, int type) {
 }
 
 // Minimize Max dist(s, C), 计算该值
-double get_select_distance(const vector<Tuple> &sites, const vector<Tuple> &centers) {
-    double max_dis = -1;
-    for (auto &site: sites) {
+// 公式1
+double get_select_distance(const vector<Tuple> &tuples, const vector<Tuple> &centers, int type = 1) {
+    double result = 0;
+    for (auto &site: tuples) {
         double min_dis = DOUBLE_MAX;
         // 选择最近的center。
         for (auto &center: centers) {
             double dis = get_distance(site, center);
             min_dis = min(min_dis, dis);
         }
-        max_dis = max(max_dis, min_dis);
+        if (type == 1) {
+            result = max(result, min_dis);
+        } else if (type == 2) {
+            result += min_dis * min_dis;
+        } else if (type == 3) {
+            result += min_dis;
+        }
     }
-    return max_dis;
+    return result;
 }
 
-vector<Tuple> center_selection(vector<Tuple> sites, int n, int k, int type) {
+// 公式2
+
+vector<Tuple> center_selection(vector<Tuple> tuples, int tuple_num, int center_num, int type) {
     vector<Tuple> centers;
-    int start = select_first_site(sites, n, type);
+    int start = select_first_site(tuples, tuple_num, type);
 #ifdef DEBUG
     cout << "Choose:" << start << '\t';
 #endif
-    vector<bool> visit(n, false);
+    vector<bool> visit(tuple_num, false);
     visit[start] = true;
 
-    centers.push_back(sites[start]);
+    centers.push_back(tuples[start]);
 
-    while (--k) {
+    while (--center_num) {
         // 选择距离当前所有centers距离最小值，最大的非center点加入center中。
         int max_distance_index = 0;
         double max_distance = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < tuple_num; ++i) {
             if (visit[i]) {   // 已经访问过的不再考虑
                 continue;
             }
 
             double min_dis = DOUBLE_MAX;
             for (auto &center: centers) {
-                double dis = get_distance(sites[i], center);
+                double dis = get_distance(tuples[i], center);
                 min_dis = min(min_dis, dis);
             }
             if (min_dis > max_distance) {
@@ -136,7 +145,7 @@ vector<Tuple> center_selection(vector<Tuple> sites, int n, int k, int type) {
                 max_distance = min_dis;
             }
         }
-        centers.push_back(sites[max_distance_index]);
+        centers.push_back(tuples[max_distance_index]);
         visit[max_distance_index] = true;
     }
     return centers;
@@ -157,34 +166,45 @@ pair<int, int> select_pair(const vector<Tuple> &sites) {
     return res;
 }
 
-vector<Tuple> distance_based_greedy_removal(vector<Tuple> sites, int tuple_num, int center_num) {
-    vector<Tuple> centers = sites;
+vector<Tuple>
+distance_based_greedy_removal(vector<Tuple> tuples, int tuple_num, int center_num, vector<int> &remove_order) {
+    vector<Tuple> centers = tuples;
     while (centers.size() > center_num) {
         auto p = select_pair(centers);
         int a = p.first, b = p.second;
         double min_a = DOUBLE_MAX, min_b = DOUBLE_MAX;
         for (int i = 0; i < centers.size(); i++) {
-            if (i != a) {
+            if (i != a && i != b) {
                 double dis = get_distance(centers[i], centers[a]);
                 min_a = min(min_a, dis);
             }
-            if (i != b) {
+            if (i != b && i != a) {
                 double dis = get_distance(centers[i], centers[b]);
                 min_b = min(min_b, dis);
             }
         }
+
+        Tuple tmp;
         if (min_a < min_b) {
+            tmp = *(centers.begin() + a);
             centers.erase(centers.begin() + a);
         } else {
+            tmp = *(centers.begin() + b);
             centers.erase(centers.begin() + b);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (tmp == tuples[i]) {
+                remove_order.push_back(i);
+            }
         }
     }
     return centers;
 }
 
-vector<Tuple> greedy_inclusion(const vector<Tuple> sites, int tuple_num, int center_num) {
-    int start = select_first_site(sites, tuple_num, 2);
-    vector<Tuple> centers = {sites[start]};
+vector<Tuple> greedy_inclusion(const vector<Tuple> tuples, int tuple_num, int center_num) {
+    int start = select_first_site(tuples, tuple_num, 2);
+    vector<Tuple> centers = {tuples[start]};
     set<int> selected = {start}; // 保存所有当前已经选择的center。
     while (selected.size() < center_num) {
         //  选择center最小化最大距离。
@@ -194,8 +214,8 @@ vector<Tuple> greedy_inclusion(const vector<Tuple> sites, int tuple_num, int cen
             if (selected.find(i) != selected.end()) {
                 continue; // 已经选过的center不考虑。
             }
-            centers.push_back(sites[i]);
-            double cur_dis = get_select_distance(sites, centers);
+            centers.push_back(tuples[i]);
+            double cur_dis = get_select_distance(tuples, centers);
             if (cur_dis < min_dis) {
                 min_dis = cur_dis;
                 selected_index = i;
@@ -203,41 +223,47 @@ vector<Tuple> greedy_inclusion(const vector<Tuple> sites, int tuple_num, int cen
             // backtrace
             centers.pop_back();
         }
-        centers.push_back(sites[selected_index]);
+        centers.push_back(tuples[selected_index]);
         selected.insert(selected_index);
     }
     return centers;
 }
 
-vector<Tuple> greedy_removal(vector<Tuple> sites, int tuple_num, int center_num) {
-    vector<Tuple> centers = sites;
+vector<Tuple> greedy_removal(vector<Tuple> tuples, int tuple_num, int center_num, vector<int> &remove_order) {
+    vector<Tuple> centers = tuples;
     while (centers.size() > center_num) {
         int length = centers.size();
         double min_dis = DOUBLE_MAX;
         int selected_index = 0;
         for (int i = 0; i < length; i++) {
-            // 每次删除一个center
+            // 每次删除一个center, 使得目标值最大
             auto tmp = centers[i];
             centers.erase(centers.begin() + i);
 
-            double cur_dis = get_select_distance(sites, centers);
+            double cur_dis = get_select_distance(tuples, centers);
             if (cur_dis < min_dis) {
                 min_dis = cur_dis;
                 selected_index = i;
             }
             centers.insert(centers.begin() + i, tmp);
         }
+
+        auto tmp = *(centers.begin() + selected_index);
+        for (int i = 0; i < n; i++) {
+            if (tmp == tuples[i]) {
+                remove_order.push_back(i);
+            }
+        }
+
         centers.erase(centers.begin() + selected_index);
+
     }
     return centers;
 }
 
-double csp(const vector<Tuple> sites, int n, int k, int type) {
-    vector<Tuple> selected_centers = center_selection(sites, n, k, type);
-    double distance = get_select_distance(sites, selected_centers);
-#ifdef DEBUG
-    cout << "type:" << type << '\t' << distance << '\t';
-#endif
+double csp(vector<Tuple> tuples, int tuple_num, int center_num, int type) {
+    vector<Tuple> selected_centers = center_selection(tuples, tuple_num, center_num, type);
+    double distance = get_select_distance(tuples, selected_centers);
     return distance;
 }
 
@@ -246,9 +272,6 @@ void compare_different_start_selection() {
         cout << k << ' ';
         vector<int> win(3, 0);
         for (int cnt = 1; cnt < epoch; cnt++) {
-#ifdef DEBUG
-            cout << "cnt:" << cnt << ' ';
-#endif
             vector<Tuple> sites(n);
             for (int i = 0; i < n; ++i) {
                 sites[i].x = distribution_coordinates(gen);
@@ -309,39 +332,50 @@ void show_centers(const vector<Tuple> &centers) {
 
 // 比较两个centers的相似度，约接近1越好。
 double compare_two_centers(const vector<int> &index1, const vector<int> &index2) {
-    set<int> intersections;
+    set<int> inter;
     for (int i = 0; i < index1.size(); ++i) {
-        intersections.insert(index1[i]);
-        intersections.insert(index2[i]);
+        inter.insert(index1[i]);
+        inter.insert(index2[i]);
     }
-    return intersections.size() * 1.0 / (index1.size() + index2.size());
+    return inter.size() * 1.0 / (index1.size() + index2.size());
 }
 
 
-void save(const vector<Tuple> &sites, int n, int k, vector<vector<int>> centers, int cnt) {
-    freopen("../alg_compare_data.txt", "w", stdout);
+void save(const vector<Tuple> &sites, int tuple_num, int center_num, vector<vector<int>> centers,
+          vector<vector<int>> remove_orders, int cnt, vector<double> distances) {
+    freopen("../alg_compare_data28.txt", "w", stdout);
 
     // 保存配置
-    cout << n << ' ' << k << endl;
+    cout << tuple_num << ' ' << center_num << endl;
 
     // 保存所有城市的下标, x坐标, y坐标。
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < tuple_num; i++) {
         cout << i << ' ' << sites[i].x << ' ' << sites[i].y << endl;
     }
 
     // 依次保存四个算法的centers
-    for (auto &center: centers) {
-        for (int j = 0; j < center.size(); j++) {
-            cout << center[j] << (j == center.size() - 1 ? '\n' : ' ');
+    for (int i = 0; i < centers.size(); i++) {
+        for (int j = 0; j < centers[i].size(); j++) {
+            cout << centers[i][j] << (j == centers[i].size() - 1 ? '\n' : ' ');
+        }
+        if (remove_orders[i].size() == 0) {
+            cout << endl;
+        }
+        for (int j = 0; j < remove_orders[i].size(); j++) {
+            cout << remove_orders[i][j] << (j == remove_orders[i].size() - 1 ? '\n' : ' ');
         }
     }
 
     cout << cnt << endl;
+
+    for (int i = 0; i < distances.size(); i++) {
+        cout << distances[i] << (i == distances.size() - 1 ? '\n' : ' ');
+    }
 }
 
 
 // 返回当前最小的交集。
-bool create_samples() {
+bool task6_1() {
     vector<Tuple> sites(n);
     set<pair<int, int>> coordinates; // 坐标去重
     for (int i = 0; i < n; ++i) {
@@ -358,17 +392,25 @@ bool create_samples() {
 
     auto alg1_centers = center_selection(sites, n, k, 2);
     auto centers1 = convert_to_index(sites, alg1_centers);
+    auto dis1 = get_select_distance(sites, alg1_centers);
 
-    auto alg2_centers = distance_based_greedy_removal(sites, n, k);
+    vector<int> alg2_remove_order;
+    auto alg2_centers = distance_based_greedy_removal(sites, n, k, alg2_remove_order);
     auto centers2 = convert_to_index(sites, alg2_centers);
+    auto dis2 = get_select_distance(sites, alg2_centers);
 
     auto alg3_centers = greedy_inclusion(sites, n, k);
     auto centers3 = convert_to_index(sites, alg3_centers);
+    auto dis3 = get_select_distance(sites, alg3_centers);
 
-    auto alg4_centers = greedy_removal(sites, n, k);
+    vector<int> alg4_remove_order;
+    auto alg4_centers = greedy_removal(sites, n, k, alg4_remove_order);
     auto centers4 = convert_to_index(sites, alg4_centers);
+    auto dis4 = get_select_distance(sites, alg4_centers);
 
     vector<vector<int>> centers = {centers1, centers2, centers3, centers4};
+    vector<double> distances = {dis1, dis2, dis3, dis4};
+    vector<vector<int>> remove_orders = {{}, alg2_remove_order, {}, alg4_remove_order};
 
     bool ok = false;
     set<int> cnt;
@@ -377,24 +419,108 @@ bool create_samples() {
             cnt.insert(centers[i][j]);
         }
     }
-    if (cnt.size() >= n - 2) {
+
+    set<double> dis_cnt;
+    for (auto dis: distances) {
+        dis_cnt.insert(dis);
+    }
+
+    if (cnt.size() >= min(n, 4 * k - 1) && dis_cnt.size() >= 4) {
         ok = true;
     }
 
     if (ok) {
-        save(sites, n, k, centers, cnt.size());
+        save(sites, n, k, centers, remove_orders, cnt.size(), distances);
     }
     return ok;
 }
 
+void task6_3_1() {
+    vector<int> xs = {1, 3, 9};
+    double min_dis = DOUBLE_MAX;
+    double res = 0;
+    for (double i = 0; i < 10; i += 0.001) {
+        double max_dis = 0;
+        for (auto x: xs) {
+            max_dis = max(max_dis, abs(x - i));
+        }
+        if (max_dis < min_dis) {
+            min_dis = max_dis;
+            res = i;
+        }
+    }
+    cout << min_dis << ' ' << res << endl;
+}
+
+void task6_3_2() {
+    vector<int> xs = {1, 3, 9};
+    double min_dis = DOUBLE_MAX;
+    double res = 0;
+    for (double i = 0; i < 10; i += 0.001) {
+        double max_dis = 0;
+        for (auto x: xs) {
+            max_dis += (abs((x - i) * (x - i)));
+        }
+        if (max_dis < min_dis) {
+            min_dis = max_dis;
+            res = i;
+        }
+    }
+    cout << min_dis << ' ' << res << endl;
+}
+
+void task6_3_3() {
+    vector<int> xs = {1, 3, 9};
+    double min_dis = DOUBLE_MAX;
+    double res = 0;
+    for (double i = 0; i < 10; i += 0.001) {
+        double max_dis = 0;
+        for (auto x: xs) {
+            max_dis += abs((x - i));
+        }
+        if (max_dis < min_dis) {
+            min_dis = max_dis;
+            res = i;
+        }
+    }
+    cout << min_dis << ' ' << res << endl;
+}
+
+void task6_4_2() {
+    vector<int> xs = {1, 4, 5, 6, 9};
+    double min_dis = DOUBLE_MAX;
+    double res1 = 0, res2 = 0;
+    for (double i = 1; i < 5; i += 0.001) {
+        for (double j = 5; j < 10; j += 0.001) {
+            double max_dis = 0;
+            for (auto x: xs) {
+                double dis1 = (x - i) * (x - i);
+                double dis2 = (x - j) * (x - j);
+                max_dis += min(dis1, dis2);
+            }
+            if (max_dis <= min_dis) {
+                min_dis = max_dis;
+                res1 = i;
+                res2 = j;
+            }
+        }
+    }
+
+    cout << min_dis << ' ' << res1 << ' ' << res2 << endl;
+}
+
 
 int main() {
-    int cnt = 0;
-    while (++cnt) {
-        auto ok = create_samples();
-        cout << cnt << endl;
-        if (ok) break;
-    }
+    task6_4_2();
+//    task6_3_1();
+//    task6_3_2();
+//    task6_3_3();
+//    int cnt = 0;
+//    while (++cnt) {
+//        cout << cnt << endl;
+//        auto ok = task6_1();
+//        if (ok) break;
+//    }
 
 //    compare_different_start_selection();
     return 0;
--- Advanced Algorithm/Center Selection Problem/show.py
+++ Advanced Algorithm/Center Selection Problem/show.py
@@ -13,16 +13,21 @@ class Tuple:
         self.y = y
 
 
-def show_centers(tuples, centers, ax, title):
+def show_centers(tuples, centers, remove, ax, title):
     x = [tuple.x for tuple in tuples]
     y = [tuple.y for tuple in tuples]
     ax.scatter(x, y, s=50, lw=0, cmap='RdYlGn', color='g')
 
     for i in range(len(centers)):
         ax.scatter(tuples[centers[i]].x, tuples[centers[i]].y, s=80, lw=0, cmap='RdYlGn', color='r')
-        ax.annotate(i + 1, xy=(tuples[centers[i]].x - 0.5, tuples[centers[i]].y + 0.5))
+        if len(remove) == 0:
+            ax.annotate(i + 1, xy=(tuples[centers[i]].x - 0.3, tuples[centers[i]].y + 0.3))
 
-    ax.axis(xmin=0, xmax=10, ymin=0, ymax=10)
+    for i in range(len(remove)):
+        ax.scatter(tuples[remove[i]].x, tuples[remove[i]].y, s=80, lw=0, cmap='RdYlGn', color='b')
+        ax.annotate(i + 1, xy=(tuples[remove[i]].x - 0.3, tuples[remove[i]].y + 0.3))
+
+    ax.axis(xmin=0, xmax=8, ymin=0, ymax=8)
     ax.grid(True)
     ax.set_title(title)
     ax.set_aspect(1)
@@ -31,7 +36,8 @@ def show_centers(tuples, centers, ax, title):
 if __name__ == '__main__':
     tuples = []
     centers = [[] for i in range(4)]
-    with open('alg_compare_data.txt', 'r') as fr:
+    removes = [[] for i in range(4)]
+    with open('alg_compare_data28.txt', 'r') as fr:
         info = fr.readline().split(' ')
         [n, k] = list(map(int, info))
         print(n, k)
@@ -45,17 +51,25 @@ if __name__ == '__main__':
             center = list(map(int, info))
             centers[i] = center
 
+            info = fr.readline().split(' ')
+            if info[0] != '\n':
+                remove = list(map(int, info))
+            else:
+                remove = []
+            removes[i] = remove
+
+    print(removes)
     fig = plt.figure(figsize=(10, 10))
     fig_row = 2
     fig_col = 2
 
     for i in range(4):
         ax = fig.add_subplot(fig_row, fig_col, i + 1)
-        show_centers(tuples, centers[i], ax, alginfo[i])
+        show_centers(tuples, centers[i], removes[i], ax, alginfo[i])
     print(centers)
 
     # plt.show()
-    plt.savefig('test_19.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'test{n}.png', bbox_inches='tight', dpi=300)
 
     # print(x, y)
     # x = range(1, 10)
