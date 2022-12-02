6b4c8caaabf358421fc8f8b6fbc7cf8c0537efe2
Qi Luo <rocky_lq@icloud.com>
2022-12-02 13:55:24 +0800

    finish load balance problem
    LP_based method

--- Advanced Algorithm/Load Balance Problem/lbp.py
+++ Advanced Algorithm/Load Balance Problem/lbp.py
@@ -1,11 +1,11 @@
 import networkx as nx
 import numpy as np
-import pulp  # 导入 pulp库
+import pulp  # 导入 pulp库 pip install pulp
 from matplotlib import pyplot as plt
 
 
 def sample():
-    lbp = pulp.LpProblem("lbp", sense=pulp.LpMinimize)  # 定义问题 1，求最大值
+    lbp = pulp.LpProblem("lbp", sense=pulp.LpMinimize)  # 定义问题，求最大值
 
     L = pulp.LpVariable("L", lowBound=0, cat="Integer")
     # 设置目标函数 f(x)
@@ -41,7 +41,7 @@ def sample():
                 cmd += f'x{j + 1}{i + 1} + '
         # job没有分配给任何machine，直接跳过，该样本不合法
         if not ok:
-            continue
+            return False
         cmd = cmd[:-3]
         cmd += f' == {weight[i]})'
         exec(cmd)
@@ -60,11 +60,12 @@ def sample():
 
     lbp.solve()
     print(lbp.name)  # 输出求解状态
-    print("Status:", pulp.LpStatus[lbp.status])  # 输出求解状态
+    # print("Status:", pulp.LpStatus[lbp.status])  # 输出求解状态
     for v in lbp.variables():
         print(v.name, "=", int(v.varValue))  # 输出每个变量的最优值
     print("F1(x)=", pulp.value(lbp.objective))  # 输出最优解的目标函数值
 
+    # 画图
     G = nx.Graph()
     plt.rcParams['figure.figsize'] = (24, 24)
 
@@ -93,6 +94,7 @@ def sample():
     job_nodes = list(G.nodes)[:job_num]
     # 取后4个节点加入到machine_nodes中
     machine_nodes = list(G.nodes)[job_num:-1]
+    # final node
     final_node = list(G.nodes)[-1]
 
     for v in lbp.variables():
@@ -121,33 +123,35 @@ def sample():
 
     # labels
     matrix = nx.to_numpy_matrix(G)
-    print(matrix)
 
-    plt.axis('off')
-    plt.savefig("net.jpg")
-    plt.show()
+    # print(matrix)
 
     # 判断无向图matrix中是否存在交叉环
     def has_cycle(matrix):
         for i in range(job_num):
             for j in range(machine_num + job_num):
                 if matrix[i, j] >= 1:
-
                     for p in range(job_num):
                         if p == i:
                             continue
-
                         for q in range(machine_num + job_num):
                             if q == j:
                                 continue
-
                             if matrix[i, q] >= 1 and matrix[p, j] >= 1 and matrix[p, q] >= 1:
                                 print(f'({i + 1}, {j + 1}) and ({p + 1}, {q + 1})')
                                 return True
 
         return False
 
+    # todo, 将交叉环以红色显示
     is_cycle = has_cycle(matrix)
+    plt.axis('off')
+    if is_cycle:
+        plt.savefig("net_with_cycle.jpg")
+    else:
+        plt.savefig("net_without_cycle.jpg")
+    # plt.show()
+    plt.clf()
     return is_cycle
 
 
